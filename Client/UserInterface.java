e3bf65199e2db3f85b3fe0dda03ab4a7e4d3023b
core-of-banyan <165307007+core-of-banyan@users.noreply.github.com>
2024-07-01 14:54:42 +0800

    t

---
+++ Client/UserInterface.java
@@ -0,0 +1,20 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+import Client.Data;
+
+class UserInterface implements Runnable {
+
+    Data data;
+
+    UserInterface(Data datat) {
+        data = datat;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
