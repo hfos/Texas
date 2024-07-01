9127d72c8057e80b8fdf7416053d43c4b63bcccf
core-of-banyan <165307007+core-of-banyan@users.noreply.github.com>
2024-07-02 04:19:00 +0800

    让test.java更能看一点

--- test/Test.java
+++ test/Test.java
@@ -298,9 +298,9 @@ class UserInterface implements Runnable {
         
         JPanel titleBar = new JPanel(new BorderLayout());
         titleBar.setBackground(bgColor);
-        titleBar.setPreferredSize(new Dimension(0, 60));
+        titleBar.setPreferredSize(new Dimension(0, 70));
 
-        JLabel titleLabel = new JLabel(" Texas Holdem Pocker");
+        JLabel titleLabel = new JLabel("");
         titleLabel.setForeground(foreColor);
         titleLabel.setFont(new Font(Font.MONOSPACED, Font.BOLD, 32));
         titleBar.add(titleLabel, BorderLayout.CENTER);
@@ -398,7 +398,7 @@ class UserInterface implements Runnable {
 
         Rectangle workArea = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
 
-        frame.setBounds(workArea.width/6,workArea.height/6,workArea.width*2/3, workArea.height*2/3);
+        frame.setBounds(300,100,1024, 600);
 
         frame.setVisible(true);
     }
